4fec920d6b9c8d9b3036a051cd0b99a26551eda6
Alok-2005 <alokchaturvedi190@gmail.com>
2025-05-10 18:12:22 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ import adminRoutes from './routes/admin.route.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -26,7 +27,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Accept']
