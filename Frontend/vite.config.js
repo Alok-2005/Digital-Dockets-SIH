5b9abec7355bfa7fb82c86d83ca04de9b94278f1
Alok-2005 <alokchaturvedi190@gmail.com>
2025-05-11 03:01:59 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'dist' // this is default, but ensure it's not changed
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
