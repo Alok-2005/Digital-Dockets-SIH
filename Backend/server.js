aeab23c24ba312dc490912213b5a6d1352744009
Alok-2005 <alokchaturvedi190@gmail.com>
2025-05-11 12:07:00 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cors({
-  origin: "http://localhost:5173" || "https://digital-dockets-sih-ggqd.vercel.app",
+  origin: "https://digital-dockets-sih-ggqd.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Accept']
